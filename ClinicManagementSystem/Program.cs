ea88087db006f412313a722fb9ab9c69b2d54eea
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-05-17 14:27:22 +0400

    adding ClinicManagementSystem project

---
+++ ClinicManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

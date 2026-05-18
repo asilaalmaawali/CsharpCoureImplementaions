d9645a73937887b5ea97f43b7cab22e8d96e1264
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-05-18 15:04:05 +0400

    System Storage  , declare variables

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace HotelManagementSystem
+﻿using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HotelManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // System Storage 
+
+            string guestName = "";
+            string guestPhone = "";
+            int roomNumber = 0;
+            string roomType = "";
+            string nightlyRate = "";
+            DateTime check_inDate;
+            DateTime check_outDate;
+            int number_of_nights = 0;
+            string room_notes = "";
+            double discount_percentage;
+            int loyaltyPoints;
+            bool flag_guest=false;
+            bool flag_checked=false;
+
+
+
+
+
+
+
+
+
+
         }
     }
 }

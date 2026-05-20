bf014206f39a3dded95dc628485e12f5d8d6ece5
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-05-20 09:32:50 +0400

    Addind System storage (variables) and LMS Main menu funtion

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -7,8 +7,42 @@ namespace LibraryManagementSystem
 {
     internal class Program
     {
+        // Storage system
+        static string MemName = "";
+        static int MembID = 0;
+        static string MemEmail = "";
+        static string membership_expiry_date = "";
+        static bool Memisregistered = false;  // whether a member is registered
+        static string BoolTitle = "";  
+        static string BookAuthor = "";
+        static string book_genre = "";
+        static int available_copies = 0;   // number of available copies
+        static bool Bookisregistered = false;  // whether a book is registered
+        static int total_books_borrowed = 0; //  total books borrowed this session
+        static int TotalFinesPaid = 0; // total fines paid this session
+       
 
 
+        public static void PrintLMSMainMenu()  // no return , no parameters
+        {
+            Console.WriteLine("LMS Main Menu:");
+            Console.WriteLine("0. Register Member");
+            Console.WriteLine("1. Display Member Profile");
+            Console.WriteLine("2. Search Book by Title");
+            Console.WriteLine("3. Borrow a Book");
+            Console.WriteLine("4. Return a Book");
+            Console.WriteLine("5. Calculate Late Fine");
+            Console.WriteLine("6. Apply Member Discount");
+            Console.WriteLine("7. Check Borrowing Eligibility");
+            Console.WriteLine("8. Register Book");
+            Console.WriteLine("9. Generate Member ID");
+            Console.WriteLine("10. Display Book Details");
+            Console.WriteLine("11. Calculate Renewal Fee");
+            Console.WriteLine("12. Update Member Email ");
+            Console.WriteLine("13. Session Summary");
+            Console.WriteLine("14. Exit");
+
+        }
 
 
 
@@ -18,22 +52,7 @@ namespace LibraryManagementSystem
             bool exit = false;
             while (exit == false)
             {
-                Console.WriteLine("Main Menu:");
-                Console.WriteLine("0. Register Member");
-                Console.WriteLine("1. Display Member Profile");
-                Console.WriteLine("2. Search Book by Title");
-                Console.WriteLine("3. Borrow a Book");
-                Console.WriteLine("4. Return a Book");
-                Console.WriteLine("5. Calculate Late Fine");
-                Console.WriteLine("6. Apply Member Discount");
-                Console.WriteLine("7. Check Borrowing Eligibility");
-                Console.WriteLine("8. Register Book");
-                Console.WriteLine("9. Generate Member ID");
-                Console.WriteLine("10. Display Book Details");
-                Console.WriteLine("11. Calculate Renewal Fee");
-                Console.WriteLine("12. Update Member Email ");
-                Console.WriteLine("13. Session Summary");
-                Console.WriteLine("14. Exit");
+                PrintLMSMainMenu();
 
                 Console.WriteLine("please select an option from the menu:");
                 int option = int.Parse(Console.ReadLine());

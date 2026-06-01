41365cb151514a286ea5c06394e97a777f4788ba
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-01 21:19:05 +0400

    Added Problem 3: Product Price Finder

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -37,11 +37,30 @@
                 Console.WriteLine(score);
             }
 
+            ////////// Problem 3: Product Price Finder 
 
 
+            double[] Prices = new double[5] {1.5 , 2 , 3.5 , 4.4 , 5};  // declare and initialize for prices array 
+
+            for (int i = 0; i < Prices.Length; i++)       // for loop and indexing
+            {
+
+                Console.WriteLine("Product " + (i + 1) + ": " + Prices[i] );   // indexing
+
+            }
 
 
+            int index = Array.IndexOf(Prices, 2);      // search for position of a value
+            if (index == -1)             // if not their will print its not found
+            {
+                Console.WriteLine("Item not found in the array");
+            }
+            else
+            {
+                Console.WriteLine("Item found at index: " + index); // 2 will find it in index 2 
+            }
 
+    
         }
     }
 }
